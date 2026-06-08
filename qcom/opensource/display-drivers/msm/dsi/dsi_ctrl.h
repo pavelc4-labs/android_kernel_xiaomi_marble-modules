73d8d9ab7a46c665623f17f33ca9301cdd430e2d
Pavelc4 <pavelc4@users.noreply.github.com>
2026-06-08 19:08:56 +0700

    display-drivers: Merge tag 'AU_TECHPACK_DISPLAY.LA.2.0.R1.00.00.00.000.140'
    From vendor tag: 'LA.VENDOR.1.0.r1-29700-WAIPIO.QSSI16.0'
    
    * https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers:
      disp: msm: dsi: Fix potential data race in ctrl isr
      disp: msm: sde: set possible clones for all encoders
      disp: msm: sde: add check to avoid multiple active CWB
      disp: msm: sde: Add change to fix slab out of bounds
      disp: msm: dp: fix double freeing of bridge memory
      msm: smmu: Unregister SMMU fault handler before cleanup

--- qcom/opensource/display-drivers/msm/dsi/dsi_ctrl.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_ctrl.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -316,11 +317,12 @@ struct dsi_ctrl {
 	bool cmd_mode;
 	u32 cmd_trigger_line;
 	u32 cmd_trigger_frame;
-	u32 cmd_success_line;
-	u32 cmd_success_frame;
+	atomic_t cmd_success_line;
+	atomic_t cmd_success_frame;
 	u32 cmd_engine_refcount;
 	u32 pending_cmd_flags;
 	bool max_hs_timer_supported;
+	atomic64_t cmd_success_ts;
 };
 
 /**
